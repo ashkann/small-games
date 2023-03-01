58c461fc6eb606da2c2ef7f8a583371e01917e11
Ashkan Nazary <a.nazary@newmotion.com>
2023-03-01 22:24:09 +0100

    Rooms via shared map wrapped inside a TVar

--- app/server.hs
+++ app/server.hs
@@ -1,74 +1,113 @@
 {-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ViewPatterns #-}
 
 import Conduit
 import Conduit qualified as C
-import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent (threadDelay)
 import Control.Concurrent.STM
 import Data.Aeson
 import Data.Binary.Builder qualified as B
 import Data.ByteString.Lazy.Char8 qualified as L
+import Data.Map qualified as M
+import Data.Text qualified as T
 import Network.Wai.EventSource.EventStream
+import Session (InMemory)
+import Session qualified as S
 import Yesod.Core
 import Yesod.EventSource
 
--- newtype Natural = Natural Int deriving (Eq, Show, Read)
+-- import TicTacToe
 
--- instance PathPiece Natural where
---   toPathPiece (Natural i) = T.pack $ show i
---   fromPathPiece s =
---     case reads $ T.unpack s of
---       (i, "") : _
---         | i < 1 -> Nothing
---         | otherwise -> Just $ Natural i
---       _ -> Nothing
+-- import Session (SessionManager(create))
+
+newtype GameId = GameId Int deriving (Eq, Show, Read)
+
+instance PathPiece GameId where
+  toPathPiece (GameId i) = T.pack $ show i
+  fromPathPiece s =
+    case reads $ T.unpack s of
+      (i, "") : _
+        | i < 1 -> Nothing
+        | otherwise -> Just $ GameId i
+      _ -> Nothing
 
 newtype Counter = Counter Int
+
+newtype Game m o = Game {events :: C.ConduitT () o m ()}
+
+createGame :: MonadIO m => m (Game m Counter)
+createGame =
+  return
+    Game
+      { events =
+          C.yieldMany [1 ..]
+            .| C.mapMC (\i -> do liftIO $ threadDelay (2 * 1000 * 1000); return i)
+            .| C.mapC Counter
+      }
+
+-- startGame :: Game m Counter -> m ()
+-- startGame (Game events) = _
 
 instance ToJSON Counter where
   toJSON (Counter c) = object ["i" .= c]
 
-events :: MonadIO m => C.ConduitT () Counter m ()
-events =
-  C.yieldMany [1 ..]
-    .| C.mapMC (\i -> do liftIO $ threadDelay (1000 * 1000); return i)
-    .| C.mapC Counter
+data Room m o = Room {game :: Game m o, chan :: TChan o}
 
-newtype App = App {ch :: TChan Counter}
+newtype App = App {rooms :: TVar (M.Map Int (Room (HandlerFor App) Counter))}
 
 mkYesod
   "App"
   [parseRoutes|
-    /sse ServerSentEventsR GET
+    /create CreateGameR POST
+    /join/#GameId JoinGameR POST
 |]
 
 instance Yesod App where
   makeSessionBackend _ = return Nothing
   shouldLogIO _ _ _ = return True
 
-getServerSentEventsR :: Handler TypedContent
-getServerSentEventsR = do
-  App {ch = source} <- getYesod
-  chann <- liftIO $ atomically $ dupTChan source
+withRooms :: InMemory Int (Room Handler Counter) a -> Handler a
+withRooms act = do
+  App {rooms = r} <- getYesod
+  liftIO . atomically $ S.run act r
+
+instance S.SessionManager Handler GameId (Room Handler Counter) where
+  create room = GameId <$> withRooms (S.create room)
+  write (GameId id) room = withRooms (S.write id room)
+  update f (GameId id) = withRooms (S.update f id)
+  delete (GameId id) = withRooms (S.delete id)
+
+postCreateGameR :: Handler TypedContent
+postCreateGameR = do
+  g@(Game events) <- createGame
+  ch <- liftIO newBroadcastTChanIO
+  _ :: GameId <- S.create $ Room g ch
+  chann <- liftIO $ atomically $ dupTChan ch
   let readCh = C.repeatMC (liftIO . atomically $ readTChan chann)
-  repEventSource (\_ -> readCh .| C.mapC toEvent)
+  let write = events .| C.mapM_C (liftIO . atomically . writeTChan ch)
+  _ <- forkHandler ($logError . T.pack . show) (C.runConduit write)
+  repEventSource (\_ -> readCh .| C.mapC toServerEvent)
   where
-    toEvent a =
+    toServerEvent a =
       ServerEvent
         { eventName = Just $ B.putStringUtf8 "Counter",
           eventId = Nothing, -- Just $ B.putStringUtf8 "1",
           eventData = [B.putStringUtf8 $ L.unpack (encode a)]
         }
 
+postJoinGameR :: GameId -> Handler TypedContent
+postJoinGameR = undefined
+
 main :: IO ()
 main = do
-  ch <- liftIO newBroadcastTChanIO
-  let write = events .| C.mapM_C (liftIO . atomically . writeTChan ch)
-  _ <- forkIO $ C.runConduit write
-  warp 3000 $ App ch
+  rooms <- newTVarIO M.empty
+  warp 3000 $ App rooms
--- lib/Session.hs
+++ lib/Session.hs
@@ -1,93 +1,31 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 
-module Session () where
+module Session (SessionManager (..), InMemory (..)) where
 
-import Control.Monad.IO.Class
-import Control.Monad.Reader
-import Control.Monad.State (runState)
-import Control.Monad.State qualified as ST
+import Control.Concurrent.STM (STM, TVar, modifyTVar, readTVar)
 import Data.Map qualified as M
-import GHC.IO.Handle (Handle, hClose, hPutStr)
-import System.IO (openTempFile)
 import Prelude hiding (id, lookup)
 
-class SessionManager m id session | m -> session id where
-  create :: session -> m id
+class SessionManager m k s | m -> k s where
+  create :: s -> m k
+  write :: k -> s -> m ()
+  update :: (s -> s) -> k -> m ()
+  delete :: k -> m ()
 
---   lookup :: id -> m (Maybe session)
---   update :: (session -> session) -> id -> m ()
---   delete :: id -> m ()
+newtype InMemory id s a = InMemory {run :: TVar (M.Map id s) -> STM a}
 
-type InMemoryBackend = ST.State (M.Map Int String)
+modify :: (M.Map id s -> M.Map id s) -> InMemory id s ()
+modify f = InMemory $ \tvar -> do modifyTVar tvar f
 
-instance SessionManager InMemoryBackend Int String where
-  create s = do
-    newId <- ST.gets $ (+ 1) . M.size
-    _ <- ST.modify $ M.insert newId s
-    return newId
+instance SessionManager (InMemory Int a) Int a where
+  create s = InMemory $ \tvar -> do
+    id <- (+ 1) . M.size <$> readTVar tvar
+    modifyTVar tvar (M.insert id s)
+    return id
 
---   lookup id = ST.gets $ M.lookup id
-
---   delete id = ST.modify $ M.delete id
-
---   update f id = do
---     s <- fmap f <$> lookup id
---     case s of
---       Just s' -> ST.modify $ M.insert id s'
---       Nothing -> return ()
-
--- x :: ST.State InMemoryBackend (Int, Int)
-x :: InMemoryBackend (Int, Int) = do
-  ix1 <- create "Ashkan"
-  ix2 <- create "Yasaman"
-  return (ix1, ix2)
-
-((i1, i2), backend) = runState x M.empty
-
-data FS = FS {root :: FilePath, template :: String}
-
-newtype FileSystem a = FileSystem (FS -> IO a)
-
-runFS :: FS -> FileSystem a -> IO a
-runFS r (FileSystem m) = m r
-
-instance Functor FileSystem where
-  fmap f (FileSystem m) = FileSystem $ fmap f . m
-
-instance Applicative FileSystem where
-  pure = FileSystem . const . pure
-  FileSystem mf <*> FileSystem ma = FileSystem $ \r -> mf r <*> ma r
-
-instance Monad FileSystem where
-  FileSystem ma >>= f = FileSystem $ \r -> do
-    a <- ma r
-    let FileSystem f' = f a
-    f' r
-
-instance SessionManager FileSystem FilePath String where
-  create s = FileSystem $ \(FS r t) -> do
-    (p, h) <- openTempFile r t
-    _ <- hPutStr h s
-    _ <- hClose h
-    return p
-
---   lookup id = liftIO $ fsRead id
-
---   update f id = undefined
---   delete id = undefined
-
--- example :: SessionManager m MemorySessionManager => m -> ()
--- example = do
---     id <- create
-
--- class Magic m where
---     doMagic :: m ()
-
--- newtype A a = A { runA :: IO a }
--- -- instance MonadIO A where
--- --     liftIO = A
-
--- instance Magic A where
---     doMagic = do
---         _ <- _
---         return ()
+  write id s = modify $ M.insert id s
+  update f id = modify $ M.update (Just . f) id
+  delete id = modify $ M.delete id
