814387dfed4a10029b7fc3104d13b79e88a27242
Ashkan Nazary <a.nazary@newmotion.com>
2023-08-12 23:04:51 +0200

    Works

--- MineSweeper/Main.hs
+++ MineSweeper/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TupleSections #-}
@@ -6,27 +7,28 @@
 module Main (main) where
 
 import Data.Array.IArray (Array, Ix (..), array, assocs, (!), (//))
+import Debug.Trace (trace)
 import Graphics.Gloss.Interface.Pure.Game (Color, Display (InWindow), Event (EventKey), Key (MouseButton), KeyState (Down, Up), Modifiers (Modifiers), MouseButton (LeftButton, RightButton), Picture (Text), black, blank, blue, circle, color, cyan, green, greyN, line, makeColor, orange, pictures, play, rectangleSolid, rectangleWire, red, scale, translate, white, yellow)
 import Prelude hiding (Left, Right)
 
-data Position = Position Float Float deriving (Eq, Ord)
+data ScreenPos = ScreenPos Float Float deriving (Eq, Ord)
 
 data Count = Zero | One | Two | Three | Four | Five | Six | Seven | Eight deriving (Eq, Enum)
 
 data Cell = Flaged | Opened Count | UnOpened
 
-data XY = XY Int Int deriving (Eq, Ord, Ix)
+data CellPos = CellPos Int Int deriving (Eq, Ord, Ix, Show)
 
-newtype Board = Board (Array XY Cell)
+newtype Board = Board (Array CellPos Cell)
 
-data World = World {board :: Board, mines :: [XY]}
+data World = World {board :: Board, mines :: [CellPos]}
 
 world0 :: World
 world0 = World (Board clean) mines
   where
-    bounds = let lower = XY 0 0; upper = XY (boardWidth - 1) (boardHeight - 1) in (lower, upper)
+    bounds = let lower = CellPos 0 0; upper = CellPos (boardWidth - 1) (boardHeight - 1) in (lower, upper)
     clean = array bounds $ map (,UnOpened) (range bounds)
-    mines = [XY 0 0, XY 0 1, XY 2 3, XY 3 3, XY 3 4, XY 4 4, XY 4 5, XY 5 5, XY 5 6]
+    mines = [CellPos 0 0, CellPos 0 1, CellPos 2 3, CellPos 3 3, CellPos 3 4, CellPos 4 4, CellPos 4 5, CellPos 5 5, CellPos 5 6]
 
 drawWorld :: World -> Picture
 drawWorld (World field _) = pictures [drawBoard field]
@@ -43,11 +45,13 @@ halfSize x = size x / 2
 blocks :: Float -> Float
 blocks x = x / blockSize
 
-fromField :: XY -> Position
-fromField (XY x y) = Position (size x) (size y)
-
-toXY :: Position -> XY
-toXY (Position x y) = XY (floor . blocks $ x) (floor . blocks $ y)
+screen2cell :: Float -> Float -> CellPos
+screen2cell x y =
+  let boardX = x + halfSize boardWidth
+      boardY = y + halfSize boardHeight
+      cellX = floor $ blocks boardX
+      cellY = floor $ blocks boardY
+   in trace (show cellX ++ ", " ++ show cellY) (CellPos cellX cellY)
 
 boardWidth :: Int
 boardWidth = 6
@@ -58,7 +62,7 @@ boardHeight = 7
 drawBoard :: Board -> Picture
 drawBoard (Board field) = center $ pictures $ map (\(xy, cell) -> at xy $ drawCell cell) (assocs field)
   where
-    at (XY x y) = translate (size x) (size y)
+    at (CellPos x y) = translate (size x) (size y)
     center = translate (negate . halfSize $ boardWidth - 1) (negate . halfSize $ boardHeight - 1)
 
 drawCell :: Cell -> Picture
@@ -94,88 +98,83 @@ square = pictures [cell, border]
     cell = color (greyN 0.7) $ rectangleSolid blockSize blockSize
     border = color white $ rectangleWire blockSize blockSize
 
--- drawCell Hidden = color blue $ rectangleSolid 10 10
-
-drawMouse :: Position -> Picture
-drawMouse (Position x y) = blank -- translate x y $ color white $ rectangleSolid 10 10
+-- drawCell Hidden = color blue $ rectangleSolid 10 10 translate x y $ color white $ rectangleSolid 10 10
 
 update :: Float -> World -> World
 update _ w = w
 
-data Adjacent = A0 | A1 | A2 | A3 | A4 | A5 | A6 | A7 deriving (Eq, Enum)
-
-adjacents :: Adjacent -> Adjacent -> XY -> [XY]
-adjacents from to xy = adj from xy : if from == to then [] else adjacents (next from) to xy
+open :: CellPos -> [CellPos] -> Board -> Board
+open p mines (Board b) = Board $ b // go [] p
   where
-    next A7 = A0
-    next a = succ a
-    adj a (XY x y) =
-      let (dx, dy) =
-            ( case a of
-                A0 -> (-1, -1)
-                A1 -> (-1, 0)
-                A2 -> (-1, 1)
-                A3 -> (0, 1)
-                A4 -> (1, 1)
-                A5 -> (1, 0)
-                A6 -> (1, -1)
-                A7 -> (0, -1)
-            )
-       in XY (x + dx) (y + dy)
+    go stop p =
+      if p `elem` stop
+        then []
+        else
+          let ps = neighbors p
+              !_ = trace ("open " ++ show p) ()
+              count = foldl (\c p -> if p `elem` mines then succ c else c) Zero ps
+           in (p, Opened count) : (if count == Zero then concatMap (go $ p : stop) ps else [])
 
-open :: XY -> [XY] -> Board -> Board
-open xy mines (Board field) = Board $ field // go xy
+flag :: CellPos -> Board -> Board
+flag pos (Board b) = Board $ b // change
   where
-    go xy = (xy, Opened $ count xy) : (if count xy == Zero then surroundings xy else [])
-      where
-        surroundings xy = foldl (\acc xy -> go xy ++ acc) [] (adj xy)
-        count xy = foldl (\c xy -> if xy `elem` mines then succ c else c) Zero (adj xy)
-        adj xy@(XY x y) = adjacents from to xy
-          where
-            (from, to)
-              | bottom && left = (A3, A5)
-              | top && left = (A5, A7)
-              | top && right = (A7, A1)
-              | bottom && right = (A1, A3)
-              | left = (A3, A7)
-              | right = (A7, A3)
-              | bottom = (A1, A5)
-              | top = (A5, A1)
-              | otherwise = (A0, A7)
-            bottom = y == 0
-            top = y == boardHeight - 1
-            left = x == 0
-            right = x == boardWidth - 1
+    change =
+      case b ! pos of
+        Flaged -> [(pos, UnOpened)]
+        UnOpened -> [(pos, Flaged)]
+        _ -> []
+
+neighbors :: CellPos -> [CellPos]
+neighbors (CellPos x y) =
+  foldl
+    ( \ps (dx, dy) ->
+        let x' = x + dx
+            y' = y + dy
+         in if (x' >= 0 && x' < boardWidth) && (y' >= 0 && y' < boardHeight)
+              then CellPos (x + dx) (y + dy) : ps
+              else ps
+    )
+    []
+    ds
+  where
+    ds =
+      [ (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1)
+      ]
 
 event :: Event -> World -> World
-event (EventKey (MouseButton LeftButton) Down (Modifiers Up Up Up) (x, y)) w@(World board mines) =
-  if xy `elem` mines then world0 else w {board = open xy mines board}
+event e w
+  | Just (x, y) <- leftClicked,
+    Just pos <- insideBoard x y =
+      let World board mines = w
+          lost = world0
+       in if pos `elem` mines then lost else w {board = open pos mines board}
+  | Just (x, y) <- rightClicked,
+    Just pos <- insideBoard x y =
+      let World board _ = w in w {board = flag pos board}
+  | otherwise = w
   where
-    xy = toXY pos
-    pos = Position (halfSize boardWidth + x) (halfSize boardHeight + y)
-event (EventKey (MouseButton RightButton) Down (Modifiers Up Up Up) (x, y)) w = flag (toXY pos) w
-  where
-    pos = Position (halfSize boardWidth + x) (halfSize boardHeight + y)
-    flag (XY x y) w@(World (Board board) _) = w {board = Board board'}
-      where
-        old = board ! XY x y
-        board' = case old of
-          Flaged -> board // [(XY x y, UnOpened)]
-          UnOpened -> board // [(XY x y, Flaged)]
-          _ -> board
-event _ w = w
-
--- event e w@(World filed position)
--- --   | Just k <- special e = case k of
--- --       KeyUp -> w
--- --       KeyDown -> w
--- --       KeyLeft -> w
--- --       KeyRight -> w
---     --   _ -> w
---    = w
---   where
---     special (EventKey (SpecialKey k) G.Down _ _) = Just k
---     special _ = Nothing
+    clicked
+      | EventKey (MouseButton btn) Down (Modifiers Up Up Up) (x, y) <- e = Just (btn, (x, y))
+      | otherwise = Nothing
+    leftClicked
+      | Just (LeftButton, xy) <- clicked = Just xy
+      | otherwise = Nothing
+    rightClicked
+      | Just (RightButton, xy) <- clicked = Just xy
+      | otherwise = Nothing
+    insideBoard x y =
+      let left = negate $ halfSize boardWidth
+          right = halfSize boardWidth
+          bottom = negate $ halfSize boardHeight
+          top = halfSize boardHeight
+       in if left <= x && x <= right && bottom <= y && y <= top then Just $ screen2cell x y else Nothing
 
 windowWidth :: Int
 windowWidth = 500
