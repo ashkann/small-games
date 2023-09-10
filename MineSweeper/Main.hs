400c9ab229fadeba989198f88d41762677377966
A.Nazary <a.nazary@shell.com>
2023-09-10 17:18:32 +0200

    Restart button works

--- MineSweeper/Main.hs
+++ MineSweeper/Main.hs
@@ -7,9 +7,38 @@
 
 module Main (main) where
 
+import Control.Monad.IO.Class (MonadIO)
 import Data.Array.IArray (Array, Ix (..), array, assocs, (!), (//))
 import Debug.Trace (trace)
-import Graphics.Gloss.Interface.Pure.Game (Color, Display (InWindow), Event (EventKey), Key (MouseButton), KeyState (Down, Up), Modifiers (Modifiers), MouseButton (LeftButton, RightButton), Picture (Text), black, blue, circle, color, cyan, green, greyN, line, makeColor, orange, pictures, play, rectangleSolid, rectangleWire, red, scale, translate, white, yellow)
+import Graphics.Gloss.Interface.IO.Game
+  ( Color,
+    Display (InWindow),
+    Event (EventKey),
+    Key (MouseButton),
+    KeyState (Down, Up),
+    Modifiers (Modifiers),
+    MouseButton (LeftButton, RightButton),
+    Picture (Text),
+    black,
+    blue,
+    circle,
+    color,
+    cyan,
+    green,
+    greyN,
+    makeColor,
+    orange,
+    pictures,
+    playIO,
+    rectangleSolid,
+    rectangleWire,
+    red,
+    scale,
+    translate,
+    white,
+    yellow,
+  )
+import Graphics.UI.GLUT.Fonts qualified as GLUT
 import Prelude hiding (Left, Right)
 
 data ScreenPos = ScreenPos Float Float deriving (Eq, Ord)
@@ -31,8 +60,8 @@ world0 = World (Board clean) mines
     clean = array bounds $ map (,UnOpened) (range bounds)
     mines = [CellPos 0 0, CellPos 0 1, CellPos 2 3, CellPos 3 3, CellPos 3 4, CellPos 4 4, CellPos 4 5, CellPos 5 5, CellPos 5 6]
 
-drawWorld :: World -> Picture
-drawWorld (World field _) = pictures [drawBoard field]
+drawWorld :: MonadIO m => World -> m Picture
+drawWorld (World field _) = do restart <- drawRestart; return $ pictures [drawBoard field, restart]
 
 size :: (Real a) => a -> Float
 size x = realToFrac x * cellSize
@@ -49,7 +78,7 @@ screen2cell x y =
       boardY = y + halfSize boardHeight
       cellX = floor $ blocks boardX
       cellY = floor $ blocks boardY
-   in trace (show cellX ++ ", " ++ show cellY) (CellPos cellX cellY)
+   in CellPos cellX cellY
 
 drawCell :: Cell -> Picture
 drawCell Flaged = pictures [square, flag] where flag = color red $ rectangleSolid 10 10
@@ -84,11 +113,6 @@ square = pictures [cell, border]
     cell = color (greyN 0.7) $ rectangleSolid cellSize cellSize
     border = color white $ rectangleWire cellSize cellSize
 
--- drawCell Hidden = color blue $ rectangleSolid 10 10 translate x y $ color white $ rectangleSolid 10 10
-
-update :: Float -> World -> World
-update _ w = w
-
 open :: CellPos -> World -> World
 open p0 w@(World (Board b0) mines) = w {board = Board $ go b0 p0}
   where
@@ -96,7 +120,7 @@ open p0 w@(World (Board b0) mines) = w {board = Board $ go b0 p0}
       | b ! p /= UnOpened = b
       | otherwise = do
           let ps = neighbors p
-              !_ = trace ("open " ++ show p) ()
+              -- !_ = trace ("open " ++ show p) ()
               count = foldl (\c p -> if p `elem` mines then succ c else c) Zero ps
               b' = b // [(p, Opened count)]
            in if count /= Zero then b' else foldl go b' ps
@@ -139,6 +163,7 @@ neighbors (CellPos x y) =
 
 event :: Event -> World -> World
 event e w
+  | Just (x, y) <- leftClicked, Just _ <- insideRestart x y = world0
   | Just (x, y) <- leftClicked,
     Just p <- insideBoard x y =
       let lost = world0 in if isMine p w then lost else open p w
@@ -171,13 +196,41 @@ drawBoard (Board b) = center $ pictures $ map (\(p, cell) -> at p $ drawCell cel
 
 insideBoard :: Float -> Float -> Maybe CellPos
 insideBoard x y =
-      let left = negate $ halfSize boardWidth
-          right = halfSize boardWidth
-          bottom = negate $ halfSize boardHeight
-          top = halfSize boardHeight
-       in if left <= x && x <= right && bottom <= y && y <= top then Just $ screen2cell x y else Nothing
+  let left = negate $ halfSize boardWidth
+      right = halfSize boardWidth
+      bottom = negate $ halfSize boardHeight
+      top = halfSize boardHeight
+   in if left <= x && x <= right && bottom <= y && y <= top then Just $ screen2cell x y else Nothing
 
--- data Game = Game { }*
+restartWidth :: Int
+restartWidth = 100
+
+restartHeight :: Int
+restartHeight = 50
+
+restartMargin :: Int
+restartMargin = 20
+
+insideRestart :: Float -> Float -> Maybe CellPos
+insideRestart x y =
+  let right = fromIntegral $ restartWidth `div` 2
+      left = negate right
+      top = fromIntegral $ (windowHeight `div` 2) - restartMargin :: Float
+      bottom = top - fromIntegral restartHeight
+   in if left <= x && x <= right && bottom <= y && y <= top then Just $ CellPos 0 0 else Nothing
+
+drawRestart :: MonadIO m => m Picture
+drawRestart = do
+  let h2 = restartHeight `div` 2
+      wh2 = windowHeight `div` 2
+      c = white
+      y = fromIntegral $ wh2 - (h2 + restartMargin)
+   in return
+        ( color c $
+            translate 0 y $
+              pictures
+                [rectangleWire (fromIntegral restartWidth) (fromIntegral restartHeight)]
+        )
 
 windowWidth :: Int
 windowWidth = 500
@@ -189,12 +242,12 @@ cellSize :: Float
 cellSize = 30
 
 main :: IO ()
-main =
-  play
+main = do
+  playIO
     (InWindow "Mine Sweeper" (windowWidth, windowHeight) (10, 10))
     black
     5
     world0
     drawWorld
-    event
-    update
+    (\e w -> return $ event e w)
+    (\_ w -> return w)
